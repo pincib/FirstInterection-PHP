aff4cd927d5b7386df6024f89cce8ef561346276
pincib <bernardo.pinciara@gmail.com>
2026-08-26 11:56:26 -0300

    .

---
+++ BasicPHP/Class03/index.php
@@ -0,0 +1,10 @@
+<?php
+    /*String*/
+    $name = 'Bernardo';
+    echo 'Meu nome é <br/>';
+    echo $name;
+    /*Integer*/
+    $age = 20;
+    echo '<br/>Minha idade é ';
+    echo $age;
+?>
---
+++ BasicPHP/Class04/index.php
@@ -0,0 +1,9 @@
+<?php
+    $name = 'Bernardo';
+    $age = 20;
+    $favsub = 'Math';
+    /*
+        Sempre tem que vir a caralha de um ponto (.) depois de jogar a variável para poder ocorrer a concatenação de forma correta!
+    */
+    echo 'My name is '.$name.' and I am '.$age.' years old and my favorite subject is '.$favsub;
+?>
