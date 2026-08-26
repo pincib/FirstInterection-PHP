7d5454b6a9ccc370b3f6a4677cd6b65487dc15e8
pincib <bernardo.pinciara@gmail.com>
2026-08-26 10:50:31 -0300

    aula 01

---
+++ BasicPHP/index.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <title>Aula 01</title>
+    <meta charset="utf-8">
+</head>
+<body>
+
+    <p>
+        <?php
+        echo "Hello world";
+        ?>
+    </p>
+
+</body>
+
+</html>
