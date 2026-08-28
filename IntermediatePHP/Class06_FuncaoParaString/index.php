8640af9fd4009b5a8c2669421d641bb5773bba60
pincib <bernardo.pinciara@gmail.com>
2026-08-27 21:23:50 -0300

    .

--- IntermediatePHP/Class06_FuncaoParaString/index.php
+++ IntermediatePHP/Class06_FuncaoParaString/index.php
@@ -35,6 +35,7 @@
 
 //teste para verificar a diferença entre echo e print_r
     $letters = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+    $letters = range('a', 'z'); // Gera o array automaticamente
     print_r($letters);
     echo '<hr>';
     $total = count($letters);
